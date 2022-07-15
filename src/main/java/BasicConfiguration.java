37d76df8b2a8a0fedba995598615602f3ad5f09c
relesi <renatolessa.2011@hotmail.com>
2022-07-14 22:57:10 -0300

    config webAdapter

---
+++ src/main/java/BasicConfiguration.java
@@ -0,0 +1,38 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/auth")
+                    .hasAuthority("SCOPE_teste")
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                    .oauth2ResourceServer()
+                    .jwt();
+                   // .jwtAuthenticationConverter(getJwtAuthenticationConverter());
+    }
+
+//    JwtAuthenticationConverter getJwtAuthenticationConverter() {
+//        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+//        converter.setAuthoritiesClaimName("authorities");
+//        converter.setAuthorityPrefix("");
+//        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+//        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+//        return authenticationConverter;
+//    }
+}
+
